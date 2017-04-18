93367810388745ac7e61e37d18a2272d0ba1e4de
Joe Shepherd <joeshepmedia@gmail.com>
2017-04-18 11:35:00 -0500

    Add delete show functionality

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,17 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getShows, getShow, getShowFaves } = require('../controllers/showCtrl');
+const { 
+  getShows, 
+  getShow, 
+  getShowFaves, 
+  addShow, 
+  deleteShow } = require('../controllers/showCtrl');
 
 router.get('/shows', getShows);
+router.post('/shows/new', addShow)
 router.get('/shows/favorites', getShowFaves);
 router.get('/shows/:id', getShow);
+router.delete('/shows/:id', deleteShow);
 
 module.exports = router;
